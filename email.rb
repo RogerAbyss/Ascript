616be3c81ddbb6e082855dc3391a991974e0d417
abyss <roger_ren@qq.com>
2017-07-01 10:07:16 +0800

    update自动上传msg ruby email自动发送

---
+++ email.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'mail' 
+ 
+smtp = {
+ :address => 'smtp.qq.com', 
+ :port => 25, 
+ :domain => 'qq.com', 
+ :user_name => '492764029', 
+ :password => "xfbjnudclefvbgei",
+ :enable_starttls_auto => true,
+ :openssl_verify_mode => 'none', 
+}
+
+Mail.defaults { delivery_method :smtp, smtp}
+
+mail = Mail.new do
+  from '492764029@qq.com'
+  to 'roger_ren@qq.com'
+  subject "test"
+  body 'body'
+end
+
+mail.deliver!
