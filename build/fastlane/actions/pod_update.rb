9c7a22dfb8426f8e6b1c9a7ff7caa2cab9da13b9
abyss <roger_ren@qq.com>
2017-07-01 21:43:24 +0800

    update 优化脚本路径 ################ build命令簇雏形 环境 ruby,rubygem,email,fastlane,fir-cli 1.证书管理 2.自动构建 3.E-Mail通知 4.自动上传 Appstore/Fir (不填写资料,不上传照片) ################

--- build/fastlane/actions/pod_update.rb
+++ build/fastlane/actions/pod_update.rb
@@ -1,8 +1,9 @@
+# 国内速度太慢，使用--no-repo-update节约大量的时间
 module Fastlane
   module Actions
     class PodUpdateAction < Action
       def self.run(params)
-      	Actions.sh "pod update --no-repo-update"
+      	Actions.sh "pod update --no-repo-update --verbose"
         UI.current.log.info "Successfully pod updale ⬆️ ".green
       end
 
