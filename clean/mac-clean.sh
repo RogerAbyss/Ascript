91a35431d7d3753fa1e03e4044eb73261cfa0c2d
abyss <roger_ren@qq.com>
2017-07-23 18:26:22 +0800

    clean

---
+++ clean/mac-clean.sh
@@ -0,0 +1,11 @@
+echo -e '\033[32m 　　　＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿
+彡　 _／ ⌒ ⌒ 
+　　/)) (●) (●) 　ヽ	 	what are u saying?
+　 ｜∩　(_人_)　 |     	 	\033[0m\033[31m☞it`s Ascipt!\033[0m\033[32m
+　 / ノ､＿ヽノ＿ノ￣) 		I`m Abyss!
+　/ /　　　　　 /ﾌ_/ 
+　L＿／＼　　　 ＼(	＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿\033[0m'
+
+echo -e '\n\033[31mClean DerivedData&Archives\033[0m'
+cd /Users/abyss/Library/Developer/Xcode/DerivedData&&rm -rf *\
+&&cd /Users/abyss/Library/Developer/Xcode/Archives&&rm -rf *&&echo -e '\033[32mClean Done!\033[0m'
